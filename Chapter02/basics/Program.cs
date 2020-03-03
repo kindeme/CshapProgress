9e4baeb620cf06aeba39eaa45dd1c6dbd4953c78
Kindeme <fkounoudji@gmail.com>
2020-03-02 20:30:39 -0600

    function

--- Chapter02/basics/Program.cs
+++ Chapter02/basics/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace basics
 {
@@ -6,7 +8,18 @@ namespace basics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // loop through the asssemblies that this app references
+            foreach(var r in Assembly.GetEntryAssembly().GetReferencedAssemblies()){
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+                // declare a varaible to count the number of methods
+                int methodCOunt = 0;
+                //  loop through all types in the assembly
+                foreach(var t in a.DefinedTypes){
+                    // add up the counts of methods
+                    methodCOunt += t.GetMethods().Count();
+                    // 
+                }
+            }
         }
     }
 }

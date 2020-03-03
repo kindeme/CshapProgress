eb2443dd823750380c73b304fdba4259bb8f9db2
Kindeme <fkounoudji@gmail.com>
2020-03-02 22:11:52 -0600

    function

--- Chapter02/basics/Program.cs
+++ Chapter02/basics/Program.cs
@@ -12,12 +12,18 @@ namespace basics
             foreach(var r in Assembly.GetEntryAssembly().GetReferencedAssemblies()){
                 var a = Assembly.Load(new AssemblyName(r.FullName));
                 // declare a varaible to count the number of methods
-                int methodCOunt = 0;
+                int methodCount = 0;
                 //  loop through all types in the assembly
                 foreach(var t in a.DefinedTypes){
                     // add up the counts of methods
-                    methodCOunt += t.GetMethods().Count();
-                    // 
+                    methodCount += t.GetMethods().Count();
+                    // output the count of types and their methods
+                    Console.WriteLine(
+                        "{0:N0} types with {1:N0} methods in {2} asssembly.", 
+                        arg0: a.DefinedTypes.Count(),
+                        arg1: methodCount,
+                        arg2: r.Name
+                    );
                 }
             }
         }

13c3c3f72a45da79efd44312eb3be3cde4a31797
Kindeme <fkounoudji@gmail.com>
2020-03-02 22:29:26 -0600

    numbers

---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // unsigned integer means positive whole number including 0
+           unint naturalNumber = 23;
+            // integer means negative or positive whole number including 0
+           int naturalNumber = -23;
+        //     float means single-precision floating point
+        // F suffix makes it a float literal
+            float realNumber = 2.3F;
+            // double means double-precision floating point
+            double anotherRealNumber = 2.3;
+        }
+    }
+}
